bfab1cb7eb972b7dc8d27379b77dc83a9dd38441
wgs06052@gmail.com <wgs06052@gmail.com>
2022-01-30 04:43:15 +0900

    packing x

---
+++ JongMan/9장/PACKING.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string.h>
+
+using namespace std;
+//PACKING
+//https://algospot.com/judge/problem/read/PACKING
+//배낭문제 유형
+//https://www.acmicpc.net/problem/12865
+
+int main(){
+    
+}
