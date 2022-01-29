9805d6a43d2c411ab6006fcd71977520bdffc50e
wgs06052@gmail.com <wgs06052@gmail.com>
2022-01-30 04:40:16 +0900

    9장

--- JongMan/9장/LIS4.cpp
+++ JongMan/9장/LIS4.cpp
@@ -4,10 +4,13 @@
 #include <string.h>
 
 using namespace std;
-
+// 최대증가부분수열을 찾기
 
 int n;
+//cache[0]에 최대수열의 길이+1 ,    cache[1]: 0번째에서 만들수있는 최대 길이,'''
+//choices[k] : k가 0일 때를 제외한 k-1번째에서 만들 수 있는 수열중 다음번째 idx
 int cache[101], S[100], choices[101];
+
 // S[start]에서 시작하는 증가부분수열중 최대길이를 반환
 int lis4(int start){
     int& ret = cache[start+1];
@@ -34,7 +37,34 @@ void reconstruct(int start, vector<int>& seq){
     if(next!=-1) reconstruct(next,seq);
 }
 
-
 int main(){
-    
+    int T; cin>>T;
+    while(T--){
+
+        cout<<endl;
+        vector<int> seq;
+        cin>>n;
+        memset(cache,-1,sizeof(cache));
+        for(int i=0;i<n;i++) cin>>S[i];
+
+        int size=lis4(-1)-1;
+        reconstruct(-1,seq);
+        for(int i=0;i<size;i++) cout<<seq[i]<<" ";
+        cout<<endl;
+
+        cout<<"cache"<<endl;     
+        for(int i=0;i<n+1;i++) cout<<cache[i]<<" ";
+        cout<<endl<<"choices"<<endl;
+        for(int i=0;i<n+1;i++) cout<<choices[i]<<" ";
+        cout<<endl;
+    }
 }
+// 4
+// 4
+// 1 2 3 4
+// 8
+// 5 4 3 2 1 6 7 8 
+// 8
+// 5 6 7 8 1 2 3 4
+// 7
+// 9 1 3 7 5 6 20
--- JongMan/9장/README.md
+++ JongMan/9장/README.md
@@ -1,2 +1,19 @@
 동적 계획법 테크닉
 ================
+# 최적화 문제의 실제 답 계산
+- DP를 사용하는 코드에서 실제 답을 계산하는 과정을 별도로 구현
+- ex) LIS를 구하는 과정에서 reconstruct()를 이용하여 실제 수열을 구한다
+
+# PROBLEM
+1. PACKING x
+2. OCR x
+3. MORSE x
+4. KLIS x
+5. DRAGON x
+6. ZIMBABWE x
+7. RESTORE x
+8. TICTACTOE x
+9. NUMBERGAME x
+10. BLOCKGAME x
+11. SUSHI x
+12. GENIUS x
